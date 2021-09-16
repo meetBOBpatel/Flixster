022755128926bc3916e7caf68d2afd5ff5da515b
Meet Patel <bobmeet1308@gmail.com>
2021-09-16 01:31:55 -0500

    Flixster Part 2

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,31 +1,48 @@
 package com.example.flixster.adapters;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
+//import android.support.v4.util.Pair;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.flixster.MainActivity;
 import com.example.flixster.R;
+import com.example.flixster.detailView.DetailActivity;
 import com.example.flixster.models.Movie;
 
 import org.jetbrains.annotations.NotNull;
+import org.parceler.Parcels;
 
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
+import static com.example.flixster.MainActivity.TAG;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
 
     Context context;
     List<Movie> movies;
+    ImageView youtubePlayIcon;
+
 
     public MovieAdapter(Context context, List<Movie> movies) {
         this.context = context;
@@ -46,6 +63,12 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Log.d("MovieAdapter", "onBindViewHolder" + position );
         Movie movie = movies.get(position);
+        if(movie.getRating() > 5.0){
+            youtubePlayIcon.setVisibility(View.VISIBLE);
+        }
+        else{
+            youtubePlayIcon.setVisibility(View.INVISIBLE);
+        }
         holder.bind(movie);
     }
 
@@ -59,12 +82,16 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
+        RelativeLayout container;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
+            youtubePlayIcon = itemView.findViewById(R.id.playButton);
+
         }
 
         public void bind(Movie movie) {
@@ -74,12 +101,41 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
 
             if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
                 imageUrl = movie.getBackDropPath();
+
             }
             else{
                 imageUrl = movie.getPosterPath();
             }
+            int radius = 40;
+            int margin = 10;
+            Glide.with(context).load(imageUrl).fitCenter().transform(new RoundedCornersTransformation(radius, margin)).into(ivPoster);
 
-            Glide.with(context).load(imageUrl).into(ivPoster);
+            // Want to click on the whole container
+            // open new activity
+
+            container.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+
+                    Log.d(TAG, "onClick: Inside onClickListener");
+
+                    Intent intent = new Intent(context, com.example.flixster.detailView.DetailActivity.class);
+
+                    intent.putExtra("movie", Parcels.wrap(movie));
+
+                    Log.d(TAG, "onClick: before starting activity");
+
+//                    Pair<View, String> p1 = Pair.create((View)tvTitle, "movieTitle");
+//                    Pair<View, String> p2 = Pair.create((View)tvOverview, "movieOverview");
+
+//                    ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) context, p1, p2);
+                    ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) context,(View)tvTitle, "movieTitle");
+                    context.startActivity(intent, options.toBundle());
+                    Log.d(TAG, "onClick: after starting activity");
+
+                    //Toast.makeText(context, container.getId(), Toast.LENGTH_SHORT).show();
+                }
+            });
 
         }
     }
--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,22 +3,32 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
     String posterPath;
     String title;
     String overview;
     String backDropPath;
+    int movieID;
+    double rating;
+
+    public Movie(){
+    }
 
     Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         backDropPath = jsonObject.getString("backdrop_path");
+        rating = jsonObject.getDouble("vote_average");
+        movieID = jsonObject.getInt("id");
+
     }
 
     public static List<Movie> fromjsonArray(JSONArray moviejsonArray) throws JSONException {
@@ -47,4 +57,11 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s", backDropPath);
     }
 
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieID() {
+        return movieID;
+    }
 }
